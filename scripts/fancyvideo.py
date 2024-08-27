2006273cd715804bebeaeaac14f2f37fea5544d5
Vince <77579722+vincedundee@users.noreply.github.com>
2024-08-27 13:39:46 +0000

    move

--- scripts/fancyvideo.py
+++ scripts/fancyvideo.py
@@ -1,7 +1,13 @@
+#FancyVideo
+import os
+import argparse
+import torch
+import yaml
+from skimage import img_as_ubyte
+from FancyVideo.fancyvideo.pipelines.fancyvideo_infer_pipeline import InferPipeline
+#//End FancyVideo
 import modules.scripts as scripts
 import gradio as gr
-import os
-
 from modules import script_callbacks
 
 
